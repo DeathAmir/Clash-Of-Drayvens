b61d80c4cbc25cdd65b3d1ed992bbd3863c7f333
DeathAmir <promoly1234@gmail.com>
2026-08-17 23:06:42 +0330

    Make server startup independent from legacy upstream version service

--- Clash SL Server/Core/Checker/VersionChecker.cs
+++ Clash SL Server/Core/Checker/VersionChecker.cs
@@ -1,17 +1,4 @@
-using System;
-using System.Diagnostics;
-using System.IO.Compression;
-using Ionic.Zip;
-using Ionic.Zlib;
-using System.IO;
-using CSS.Core.Threading;
-using static CSS.Core.Logger;
-using System.Net;
-using System.Threading;
-using System.Reflection;
 using CSS.Core.Settings;
-using Newtonsoft.Json.Linq;
-using System.Threading.Tasks;
 
 namespace CSS.Core.Web
 {
@@ -19,31 +6,19 @@ namespace CSS.Core.Web
     {
         public static void DownloadUpdater()
         {
-            WebClient client = new WebClient();
-            client.DownloadFile("https://CSS-up.000webhostapp.com/CSS_Updater.dat", @"Tools/Updater.exe");
-            Thread.Sleep(1000);
-            Process.Start(@"Tools/Updater.exe");
-            Environment.Exit(0);
+            // Legacy automatic updater intentionally disabled in Clash Of Drayvens.
+            // Updates are delivered through GitHub Releases instead.
         }
 
         public static string GetVersionString()
         {
-            try
-            {
-                WebClient wc = new WebClient();
-                string Version = wc.DownloadString("https://raw.githubusercontent.com/skyprolk/Clash-Of-SL/main/Clash%20SL%20Server/version.txt");
-                return Version;
-            }
-            catch (Exception)
-            {
-                return "Error";
-            }
+            // Keep startup deterministic and independent from the legacy upstream URL.
+            return Constants.Version;
         }
 
         public static string LatestCoCVersion()
         {
-
-                return "8.709.16";
+            return "8.709.16";
         }
     }
 }
