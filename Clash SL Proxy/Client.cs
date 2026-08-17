60a51ff1ca65be0a16a8285985ec434233964c74
DeathAmir <promoly1234@gmail.com>
2026-08-17 23:07:59 +0330

    Forward proxy traffic to Clash Of Drayvens production server

--- Clash SL Proxy/Client.cs
+++ Clash SL Proxy/Client.cs
@@ -11,33 +11,36 @@ namespace CSP
         public Client(ServerState serverstate)
         {
             state.serverState = serverstate;
-            state.clientKey   = clientKey;
-            state.serverKey   = serverKey;
+            state.clientKey = clientKey;
+            state.serverKey = serverKey;
         }
 
         public void StartClient()
         {
             try
             {
-                IPHostEntry ipHostInfo    = Dns.GetHostEntry(CSP.Proxy.hostname);
-                IPAddress ipAddress       = ipHostInfo.AddressList[0];
-                IPEndPoint remoteEndPoint = new IPEndPoint(ipAddress, CSP.Proxy.port);
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(CSP.Proxy.upstreamHost);
+                IPAddress ipAddress = Array.Find(ipHostInfo.AddressList, a => a.AddressFamily == AddressFamily.InterNetwork)
+                                      ?? ipHostInfo.AddressList[0];
+                IPEndPoint remoteEndPoint = new IPEndPoint(ipAddress, CSP.Proxy.upstreamPort);
 
-                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                state.socket  = socket;
+                Socket socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                state.socket = socket;
                 socket.Connect(remoteEndPoint);
                 socket.BeginReceive(state.buffer, 0, State.BufferSize, 0, ReceiveCallback, state);
 
                 Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("[COC]");
+                Console.Write("[DRAYVENS]");
                 Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.Write("[HOST]");
+                Console.Write("[UPSTREAM]");
                 Console.ResetColor();
-                Console.WriteLine("  The proxy is succefully linked to {0} ...", socket.RemoteEndPoint);
+                Console.WriteLine(" Connected to {0}", socket.RemoteEndPoint);
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Unable to connect to Drayvens upstream: " + e.Message);
+                Console.ResetColor();
             }
         }
     }
