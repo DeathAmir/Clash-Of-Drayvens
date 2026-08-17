aa8bccb7868b08a92d3bbc017d067612d8ceb4de
DeathAmir <promoly1234@gmail.com>
2026-08-17 23:07:10 +0330

    Rebrand server assembly metadata

--- Clash SL Server/Properties/AssemblyInfo.cs
+++ Clash SL Server/Properties/AssemblyInfo.cs
@@ -2,15 +2,15 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-[assembly: AssemblyTitle("Clash SL Server")]
-[assembly: AssemblyDescription("A .NET Server")]
+[assembly: AssemblyTitle("Clash Of Drayvens Server")]
+[assembly: AssemblyDescription("Clash Of Drayvens private server core")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Sky Production")]
-[assembly: AssemblyProduct("Clash SL Server")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCompany("Clash Of Drayvens Project")]
+[assembly: AssemblyProduct("Clash Of Drayvens Server")]
+[assembly: AssemblyCopyright("Copyright Â© 2026 Clash Of Drayvens contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(true)]
 
-[assembly: AssemblyVersion("0.7.3.5")]
-[assembly: AssemblyFileVersion("0.7.3.5")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
